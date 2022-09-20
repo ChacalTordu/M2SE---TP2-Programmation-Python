9ff715e99fbd16e602015cdc3c5eff298654f760
ChacalTordu <leolebrun6@gmail.com>
2022-09-20 10:08:34 +0200

    ajout structuration du fichier

--- lecture-affichage-image.py
+++ lecture-affichage-image.py
@@ -3,16 +3,26 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import random
 
+#-------------------------------------------------------------------
+#-------------------------------EXO 1-------------------------------
+#-------------------------------------------------------------------
 
+#Question 1
 image_RGB = Image.open('mola'+'.bmp')
 plt.imshow(image_RGB)
 plt.show()
 
+#Question 2
 array_RGB = np.array(image_RGB)
+
+#Question 3
 image_gris = image_RGB.convert('L') 
 plt.imshow(image_gris)
 plt.show()
 
+#-------------------------------------------------------------------
+#-------------------------------EXO 2-------------------------------
+#-------------------------------------------------------------------
 
 #enlever niveau de rouge
 image_test = array_RGB[:,:,1]
