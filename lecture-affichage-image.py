c91b4434381a1dc9f0e005d6ea81a5ad37a0a7bc
ChacalTordu <leolebrun6@gmail.com>
2022-09-20 12:00:03 +0200

    ajout pour la question des images avec la couleurs séparé (nr

--- lecture-affichage-image.py
+++ lecture-affichage-image.py
@@ -9,17 +9,19 @@ import random
 #-------------------------------------------------------------------
 
 #Question 1
-image_RGB = Image.open('lena'+'.bmp')
-plt.imshow(image_RGB)
+image_programme = Image.open('lena'+'.bmp')
+plt.suptitle('Image original')
+plt.imshow(image_programme)
 plt.show()
 
 #Question 2
-array_RGB = np.array(image_RGB)
+array_RGB = np.array(image_programme)
 print(np.shape(array_RGB))
 
 #Question 3
-image_gris = image_RGB.convert('L') 
-plt.imshow(image_gris)
+image_gris = image_programme.convert('L') 
+plt.suptitle('grisé')
+plt.imshow(image_gris,cmap='gray')
 plt.show()
 
 #-------------------------------------------------------------------
@@ -30,9 +32,20 @@ plt.show()
 fig, axs = plt.subplots(1, 3, figsize=(10, 3))
 fig.suptitle('Les trois images séparés')
 
-for i in [0,1,2]:
-    image_modified_color = array_RGB[:,:,i]
-    axs[i].imshow(image_modified_color)
+(largeur, hauteur)= image_programme.size
+#image_programme_modified = image_programme
+for i in range(2):
+    for x in range(largeur):
+        for y in range(hauteur):
+            (rouge,vert,bleu) = image_programme.getpixel((x,y)) # Ici on traite le pixel (x,y) de l'image.
+            if i==0:
+                rouge = 0
+            if i==1:
+                vert = 0
+            if i==2:
+                bleu = 0
+            image_programme.putpixel((x,y),(rouge,vert,bleu)) #supprime la couleur selon i
+    axs[i].imshow(image_programme)
 plt.show()
 
 
